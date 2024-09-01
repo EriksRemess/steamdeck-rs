21be385ef5e92b04e1ac852971354892c09b4de2
Ēriks Remess <eriks@remess.lv>
2024-09-01 23:23:05 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use hidapi::{HidApi, HidDevice};
 use image::codecs::jpeg::JpegEncoder;
-use image::imageops;
-use image::GenericImageView;
+use image::imageops::FilterType::Lanczos3;
+use image::imageops::{crop_imm, resize, rotate180};
+use image::{load_from_memory, GenericImageView};
+use std::cmp::min;
 use std::process::Command;
-use std::thread;
+use std::thread::sleep;
 use std::time::Duration;
-
-fn sleep(duration: u64) {
-    thread::sleep(Duration::from_millis(duration));
-}
 
 const ACTIONS: [&str; 15] = [
     "xdg-open https://www.google.com",
@@ -22,9 +20,9 @@ const ACTIONS: [&str; 15] = [
     "xdg-open https://www.amazon.com",
     "xdg-open https://www.ebay.com",
     "xdg-open https://www.netflix.com",
-    "xdg-open https://music.youtube.com",
+    "/opt/microsoft/msedge/microsoft-edge --profile-directory=Default --app-id=cinhimbnkkaeohfgghhklpknlkffjgod '--app-url=https://music.youtube.com/?source=pwa'",
     "xdg-open https://www.twitch.com",
-    "xdg-open https://teams.microsoft.com",
+    "/opt/microsoft/msedge/microsoft-edge --profile-directory=Default --app-id=cifhbcnohmdccbgoicgdjpfamggdegmo '--app-url=https://teams.microsoft.com/v2/?clientType=pwa'",
     "flatpak run com.raggesilver.BlackBox",
 ];
 
@@ -49,10 +47,12 @@ const ACTION_ICONS: [&[u8]; 15] = [
 fn get_device(vendor_id: u16, product_id: u16, usage: u16, usage_page: u16) -> Option<HidDevice> {
     let api = HidApi::new().expect("Failed to create HID API");
     for dev in api.device_list() {
-        if dev.vendor_id() == vendor_id
-            && dev.product_id() == product_id
-            && dev.usage() == usage
-            && dev.usage_page() == usage_page
+        if (
+            dev.vendor_id(),
+            dev.product_id(),
+            dev.usage(),
+            dev.usage_page(),
+        ) == (vendor_id, product_id, usage, usage_page)
         {
             if let Ok(device) = dev.open_device(&api) {
                 return Some(device);
@@ -65,16 +65,13 @@ fn get_device(vendor_id: u16, product_id: u16, usage: u16, usage_page: u16) -> O
 
 fn set_brightness(device: &HidDevice, percentage: usize) {
     let mut buf = [0u8; 32];
-    buf[0] = 0x03;
-    buf[1] = 0x08;
-    buf[2] = percentage as u8;
+    buf[0..3].copy_from_slice(&[0x03, 0x08, percentage as u8]);
     device.send_feature_report(&mut buf).unwrap();
 }
 
 fn launch_app(action: &str) {
     let path: Vec<&str> = action.split_whitespace().collect();
     let child = Command::new(&path[0]).args(&path[1..]).spawn();
-
     if let Err(e) = child {
         eprintln!("Error: {:?}", e);
     }
@@ -100,7 +97,7 @@ fn set_key_image(device: &HidDevice, key: u8) {
     let mut page_number = 0;
     let mut bytes_remaining = img.len();
     while bytes_remaining > 0 {
-        let this_length = std::cmp::min(bytes_remaining, 1024 - 8);
+        let this_length = min(bytes_remaining, 1024 - 8);
         let bytes_sent = page_number * (1024 - 8);
         let header = [
             0x02,
@@ -123,29 +120,29 @@ fn set_key_image(device: &HidDevice, key: u8) {
 }
 
 fn get_image_data(img_data: &[u8]) -> Vec<u8> {
-    let img = image::load_from_memory(img_data).unwrap();
+    let img = load_from_memory(img_data).unwrap();
     let (width, height) = img.dimensions();
-    let crop_size = std::cmp::min(width, height); // Take the smallest dimension
+    let crop_size = min(width, height);
     let x_offset = (width - crop_size) / 2;
     let y_offset = (height - crop_size) / 2;
-    let mut img = imageops::crop_imm(&img, x_offset, y_offset, crop_size, crop_size).to_image();
-    img = imageops::rotate180(&img);
-    img = imageops::resize(&img, 72, 72, imageops::FilterType::Nearest);
+    let mut img = crop_imm(&img, x_offset, y_offset, crop_size, crop_size).to_image();
+    img = resize(&rotate180(&img), 72, 72, Lanczos3);
     let mut data = Vec::new();
-    let mut encoder = JpegEncoder::new_with_quality(&mut data, 100);
-    encoder.encode_image(&img).unwrap();
+    JpegEncoder::new_with_quality(&mut data, 100)
+        .encode_image(&img)
+        .unwrap();
     data
 }
 
 fn main() {
     if let Some(device) = get_device(0x0fd9, 0x0080, 0x0001, 0x000c) {
-        set_brightness(&device, 100);
+        set_brightness(&device, 60);
         for i in 0..15 {
             set_key_image(&device, i);
         }
         loop {
             read_states(&device);
-            sleep(100);
+            sleep(Duration::from_millis(100));
         }
     };
 }
